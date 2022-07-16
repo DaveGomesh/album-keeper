f0dc59cdef69582e85d5b4acfe8e60ef3340e9eb
David Gomesh <davidgomesh2016@gmail.com>
2022-07-16 09:22:51 -0300

    Added Spring Boot 2.7.1

---
+++ src/main/java/edu/ifma/keeper/AlbumKeeperApplication.java
@@ -0,0 +1,13 @@
+package edu.ifma.keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifma/keeper/AlbumKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifma.keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
