02c91e1b141a33134f83c62d6e81804ef94beaea
David Gomesh <davidgomesh2016@gmail.com>
2022-07-17 21:05:29 -0300

    Added RegraNegocio Exception e Handler

---
+++ src/main/java/edu/ifma/keeper/domain/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package edu.ifma.keeper.domain.exception;
+
+public class RegraNegocioException extends RuntimeException {
+    
+    public RegraNegocioException(String mensagem) {
+		super(mensagem);
+	}
+}
